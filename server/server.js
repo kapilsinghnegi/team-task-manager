b9f3724651b2d32a8a8ff48c5851c7e3f6126e4d
Kapil Singh Negi <124447041+kapilsinghnegi@users.noreply.github.com>
2026-05-14 23:17:04 +0530

    feat: connect to DB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import app from "./src/app.js";
-const PORT = process.env.PORT || 5000;
+import connectDB from "./src/config/db.js";
 
+const PORT = process.env.PORT || 5000;
+try {
+  connectDB();
+} catch (error) {
+  process.exit(1);
+}
+connectDB();
 app.listen(PORT, () =>
   console.log(`Team Task Manager is running on port ${PORT}`),
 );
