de5da309deb5c06f00645cbfb69fd75b71fa8332
Kapil Singh Negi <124447041+kapilsinghnegi@users.noreply.github.com>
2026-05-15 12:32:16 +0530

    feat: initialize project with Vite, React, and shadcn/ui

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold text-red-600 mb-2">
+        Team Task Manager
+      </h2>
+      <Button>Click here</Button>
+    </div>
+  );
+}
+
