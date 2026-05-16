2b1210c3d15dba004756b5852a56ef01dd3509c5
Kapil Singh Negi <124447041+kapilsinghnegi@users.noreply.github.com>
2026-05-16 23:04:17 +0530

    Fix prod cookie authentication issue

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -69,12 +69,10 @@ const loginUser = async (req, res) => {
 
     const token = await user.generateAuthToken();
     res.cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-      ),
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({
